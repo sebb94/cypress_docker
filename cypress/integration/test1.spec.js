8f13e00d9a34ebb4848f0b3c5107bbc7a20fd5e5
sebb94 <omgl0lwth@gmail.com>
2020-09-16 12:35:21 +0200

    docker configuration and run tests

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,5 +1,10 @@
 describe('Test', () => {
     it('Test step', () => {
-        
+        cy.visit('http://example.com')
+    });
+
+    it('should load h1 element', () => {
+        cy.get('h1').should('be.visible')
+        cy.get('p').should('be.visible')
     });
 });
